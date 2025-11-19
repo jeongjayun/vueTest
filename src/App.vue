5e62d9697bd71b12b04f67a061553da3e32b425b
jeongjayun <116062065+jeongjayun@users.noreply.github.com>
2025-11-19 16:54:53 +0900

    1. App.vue  파일의 구조 2. v-for 문법 3.데이터 바인딩 4.속성값 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,60 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+// 스크립트
+export default {
+  name: 'App',
+  data() {
+    return {
+      foods: ['김밥', '치즈만두', '햄버거', '아이스 아메리카노 얼음 많이'],
+      data: [
+        {
+          title: '노량',
+          year: 2023,
+          category: '액션, 드라마',
+          textRed: 'color:red',
+        },
+        {
+          title: '부리부리 대마왕과 짱구의 모험',
+          year: 2024,
+          category: '드라마',
+          textRed: 'color:blue',
+        },
+        {
+          title: '키보드 익숙해지기가 쉽지 않다',
+          year: 2025,
+          category: '기술, 개발',
+          textRed: 'color:green',
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <!-- html 코드 작성되는 부분 -->
+  <H1>영화 정보</H1>
+  <div v-for="(movie, i) in data" :key="i">
+    <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
+    <p>개봉 : {{ movie.year }}</p>
+    <p>장르 : {{ movie.category }}</p>
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <!--
+      note 1: 데이터 바인딩 할 때는 중괄호 안에 변수를 적어준다.
+      note 2: ':속성명'="데이터" 속성에 값을 바인딩할 때 사용. 뷰 문법임.
+    -->
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!--<p v-for="(item, i) in foods" :key="i">{{ item }}</p> -->
+
+  <!--
+     note 3: 반복문법 v-for
+  -->
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+/* 스타일 */
+.bg-yellow {
+  background: gold;
+  padding: 20px;
 }
 </style>
